64dcc233bb77d5b1aa82abaedbe63f5c1f8f612a
nikkibrooks2389 <nbrooks2389@gmail.com>
2024-03-06 10:09:32 -0800

    remove clear items

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
 
 const addItem = (e) => {
     e.preventDefault();
@@ -39,5 +40,19 @@ const createIcon = (classes) => {
     return icon;
 }
 
+const removeItem = (e) => {
+    if (e.target.parentElement.classList.contains('remove-item')) {
+        e.target.parentElement.parentElement.remove();
+    }
+}
+
+const clearItems = () => {
+    while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild);
+    }
+}
+
 //Event Listeners
-itemForm.addEventListener('submit', addItem);   
+itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem); //Event Delegation 
+clearBtn.addEventListener('click', clearItems);
